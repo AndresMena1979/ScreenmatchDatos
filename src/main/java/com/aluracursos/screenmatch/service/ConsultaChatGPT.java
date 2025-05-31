be9dd1812d6076a776b3570ba94823d1b0200a2e
Andres Mena G <afmg19791130@hotmail.com>
2025-05-30 21:25:44 -0500

    1ra parte persistencia de datos

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -6,7 +6,7 @@ public class ConsultaChatGPT {
 
 
         public static String obtenerTraduccion(String texto) {
-            OpenAiService service = new OpenAiService("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");  //ApiKey de openai
+            OpenAiService service = new OpenAiService("OPENAI_API_KEY");  //ApiKey de openai
 
             CompletionRequest requisicion = CompletionRequest.builder()
                     .model("gpt-3.5-turbo")
